35c3ad965583f05feae070201b6c1adbdbd02cac
Francisco Silveira <franciscosilveira@ua.pt>
2021-04-13 20:08:54 +0100

    Logging to kafka

--- opensky/src/main/java/andr/springframework/opensky/OpenskyApplication.java
+++ opensky/src/main/java/andr/springframework/opensky/OpenskyApplication.java
@@ -1,13 +1,19 @@
 package andr.springframework.opensky;
 
+import andr.springframework.opensky.controllers.FlightController;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OpenskyApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		SpringApplication.run(OpenskyApplication.class, args);
+		Timer timer = new Timer();
+		timer.log();
+
 	}
 
 }
