93025e6d7e2f878b10d0ecb3391fdd338ea85691
Francisco Silveira <franciscosilveira@ua.pt>
2021-04-13 20:10:44 +0100

    changed topic to opensky-logs

--- opensky/src/main/java/andr/springframework/opensky/OpenskyApplication.java
+++ opensky/src/main/java/andr/springframework/opensky/OpenskyApplication.java
@@ -1,8 +1,5 @@
 package andr.springframework.opensky;
 
-import andr.springframework.opensky.controllers.FlightController;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
